e28f8f56035802c3e4c9b060bd761837f7cc451b
Jesa Dileep Philip <philipdileepjesa@gmail.com>
2025-07-31 16:01:15 +0530

    Login page

--- innovatech/src/App.jsx
+++ innovatech/src/App.jsx
@@ -1,21 +1,19 @@
-import './App.css'
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import AuthorityDashboard from '../src/pages/AuthorityDashboard';
-import PublicDashboard from '../src/pages/PublicDashboard';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import AuthorityDashboard from './pages/AuthorityDashboard';
+import PublicDashboard from './pages/PublicDashboard';
+import './App.css';
+
 function App() {
   return (
-    <>
-      <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/authority" element={<AuthorityDashboard/>} />
-            <Route path="/dashboard" element={<PublicDashboard />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/authority" element={<AuthorityDashboard />} />
+        <Route path="/dashboard" element={<PublicDashboard />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
