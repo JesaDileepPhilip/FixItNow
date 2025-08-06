52e28a83f6d7a40f434df7aaf22c93fea3854085
Jesa Dileep Philip <philipdileepjesa@gmail.com>
2025-08-06 22:14:31 +0530

    Last

---
+++ innovatech/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthorityDashboard from './pages/AuthorityDashboard';
+import './App.css'
+import React from 'react';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import ReportIssue from './pages/ReportIssue';
+import PublicDashboard from './pages/PublicDashboard';
+import Navbar from './components/Navbar';
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/authority" element={<AuthorityDashboard />} />
+        <Route path="/dashboard" element={<PublicDashboard />} />
+        <Route path="/" element={<Login />} />
+        {/*<Route path="/login" element={<Login />} />*/}
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
