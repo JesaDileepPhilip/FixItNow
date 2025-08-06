cb711b23cfd78a98133f8b147ed89fbedc0c76e1
Diya Therese Shibu <diyathereseshibu@gmail.com>
2025-08-06 20:08:14 +0530

    corrected image

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routes import dashboard
+
 app = FastAPI()
+
+# CORS setup
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include routers
+app.include_router(dashboard.router)
